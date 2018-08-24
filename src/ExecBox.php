c62308e78818140e569ef7f2ca02a5f40ea5024e
Matthias <m@tth.es>
2018-08-24 13:11:07 +0200

    added interactive terminal

--- src/ExecBox.php
+++ src/ExecBox.php
@@ -33,6 +33,30 @@ class ExecBox
             $rProc = proc_open("bash -c 'trap \"kill 0\" EXIT; $cmd 2>&1 > /dev/tty & wait' & echo $! > /tmp/curpid.kick", [], $aPipes);
             proc_close($rProc);
             $this->pids[] = (int) file_get_contents("/tmp/curpid.kick");
+        } else if (preg_match("/^IT\:(.*)$/", $cmd, &$matches)) {
+            // Interactive Terminal (Ncurses etc)
+            $cmd = $matches[1];
+            $pipes = array (NULL, NULL, NULL);
+            // Allow user to interact with dialog
+            $in = fopen ('php://stdin', 'r');
+            $out = fopen ('php://stdout', 'w');
+            // But tell PHP to redirect stderr so we can read it
+            $p = proc_open ($cmd, array (
+                0 => $in,
+                1 => $out,
+                2 => array ('pipe', 'w')
+            ), $pipes);
+            // Wait for and read result
+            $result = stream_get_contents ($pipes[2]);
+            // Close all handles
+            fclose ($pipes[2]);
+            fclose ($out);
+            fclose ($in);
+            $ret = proc_close ($p);
+            if ($ret != 0) {
+                Out::fail("Command '$cmd' returned exit-code: '$ret'");
+                exit ($ret);
+            }
         } else {
             Out::log("Exec syncronously: '$cmd'");
             system($cmd, $ret);
